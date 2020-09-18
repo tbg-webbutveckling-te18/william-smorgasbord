c5222b1b695cd42d3fd515fc8b674deb82cc76fc
William Olsson <burrken55@gmail.com>
2020-09-18 15:07:10 +0200

    First lesson with JS

---
+++ projects/web2/2-JavaScript/assets/js/1-ifelse.js
@@ -0,0 +1,24 @@
+
+var age = 19;
+if (age >= 18) {
+    console.log('Du får ta körkortet.')
+} else {
+    console.log('Du får inte ta körkortet.')
+}
+
+
+var lesson = 'web development';
+if (lesson == 'history'){
+    console.log('We have:' + lesson)
+} else if (lesson == 'web development'){
+    console.log('we have: ' + lesson);
+} else {
+    console.log('spare time!');
+}
+
+var isSunnny = false;
+if (isSunny == true){
+    console.log('Put on some shorts today.');
+} else {
+    console.log('Put on some warm clothes today.');
+}
